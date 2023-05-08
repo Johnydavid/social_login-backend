1f5c7f2e40e25d3756edead8a21d0d67d9411cd6
Johnson David <92262280+Johnydavid@users.noreply.github.com>
2023-05-08 18:14:03 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ app.use(
 
 app.use(express.urlencoded({ extended: false }));
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://guvi-socialmedia.netlify.app/" );
+  res.header("Access-Control-Allow-Origin", "*" );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
