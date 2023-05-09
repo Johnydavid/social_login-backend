3e7a4d5e4736f3b79f570ea43f6b05f275fb04c3
Johnydavid <dropin2johny@gmail.com>
2023-05-09 17:57:06 +0530

    c

--- server/passport.js
+++ server/passport.js
@@ -54,8 +54,8 @@ passport.use(
     {
       clientID: GITHUB_CLIENT_ID,
       clientSecret: GITHUB_CLIENT_SECRET,
-      callbackURL: "https://social-media-login.onrender.com/auth/github/callback",
-      // callbackURL: "http://localhost:8080/auth/github/callback"
+      // callbackURL: "https://social-media-login.onrender.com/auth/github/callback",
+      callbackURL: "http://localhost:8080/auth/github/callback"
       // callbackURL: "https://guvi-socialmedia.netlify.app/auth/github/callback",
     },
     function (accessToken, refreshToken, profile, done) {
