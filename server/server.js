2fe1a6eadd1ceaa16efca55e14a1ca555bd7a0c0
Johnydavid <dropin2johny@gmail.com>
2023-06-25 11:06:15 +0530

    c

--- server/server.js
+++ server/server.js
@@ -31,12 +31,12 @@ app.use(
 
 app.use(express.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://guvi-socialmedia.netlify.app/"
-  );
-  // res.header("Access-Control-Allow-Origin", "*");
-  changeOrigin: true,
+  // res.header(
+  //   "Access-Control-Allow-Origin",
+  //   "https://guvi-socialmedia.netlify.app/"
+  // );
+  res.header("Access-Control-Allow-Origin", "*");
+  // changeOrigin: true,
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
